63349287b40ccac8f93c08ffcb902c52931dcf0a
Abdul Daniel Trato <abdultrato@gmail.com>
2026-03-01 20:24:21 +0200

    6
    6

--- aplicativos/clinico/apps.py
+++ aplicativos/clinico/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
 
-class ClinicoConfig(AppConfig) :
-	default_auto_field = "django.db.models.BigAutoField"
-	name = "aplicativos.clinico"
-	verbose_name = "Gestão Clínica"
-	
-	def ready(self) :
-		pass
+class ClinicoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "aplicativos.clinico"
+    verbose_name = "Gestão Clínica"
+
+    def ready(self):
+        pass
